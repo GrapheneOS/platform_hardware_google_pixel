d26f40b09162550a0eac2172c19c089fd5f50de0
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-07-22 12:36:31 +0300

    replace AOSP CarrierConfig with GrapheneOS CarrierConfig2

--- common/pixel-common-device.mk
+++ common/pixel-common-device.mk
@@ -68,3 +68,10 @@ PRODUCT_PACKAGES += com.android.hardware.biometrics.face.virtual
 ifeq ($(RELEASE_INSTALL_AI_LABS_ARTIFACTS), true)
   include vendor/google/AILabs/ai_labs.mk
 endif
+
+DEVICE_PACKAGE_OVERLAYS += hardware/google/pixel/common/overlay
+
+ifneq ($(BOARD_WITHOUT_RADIO),true)
+    PRODUCT_PACKAGES += \
+        CarrierConfig2
+endif
